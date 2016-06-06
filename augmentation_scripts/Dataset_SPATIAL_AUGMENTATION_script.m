c2e8cb82c79b2a710185ce628e9598123d29c0b0
jing <jinglu.sherry@gmail.com>
2016-06-06 20:46:35 +0200

    revise augmentation code

--- augmentation_scripts/Dataset_SPATIAL_AUGMENTATION_script.m
+++ augmentation_scripts/Dataset_SPATIAL_AUGMENTATION_script.m
@@ -1,242 +1,309 @@
-clear all;
-close all;
+%% image augmentation including?
+%% jinglu.sherry@gmail.com
 
+%% step 1, crop the images
+%% step 2. calculate mean
+
+clear all; close all; clc
+
+addpath(genpath('/home/luj/code/deepscene')
+
+% image dimention
 IMAGE_DIM=350;
 CROP_DIM = 300;
+
+
+% dataset and mask directory
+DIR_DATASET = 'dataset/';
+DIR_MASK = 'masks/';
+
+% Read files names in the folder
+origin_imgs = dir(DIR_DATASET);
+mask_imgs = dir(DIR_MASK);
+
+% get number of images
+imgs_num = length(origin_imgs);
+mask_num = length(mask_imgs);
+
 % mean BGR pixel
-mean_pix = [104.00698793, 116.66876762, 122.67891434];
+% mean_pix = [104.00698793, 116.66876762, 122.67891434];
+mean_pix = calculate_mean_pix(DIR_DATASET,origin_imgs,IMAGE_DIM,CROP_DIM);
 
-
+% aug_mum?
 aug_num = 25;
 
-%Cityscapes dataset
-DIR='dataset/';
+Img = zeros([CROP_DIM CROP_DIM 3 ((imgs_num)*(aug_num))]);
+Seg = zeros([CROP_DIM CROP_DIM 1 ((mask_num)*(aug_num))]);
 
-files=dir(DIR);
-
-loop=length(files);
-
-%Img=zeros([CROP_DIM CROP_DIM 3 ((loop-2)*aug_num)]);
-Img=zeros([CROP_DIM CROP_DIM 3 ((loop-2)*(aug_num))]);
 
 index=0;
 
-
-
-DIR2='masks/';
-
-files2=dir(DIR2);
-
-loop2=length(files2);
-
-Seg=zeros([CROP_DIM CROP_DIM 1 ((loop2-2)*(aug_num))]);
-
-
-%loop first 10
-for p=3:loop
-    p-2
-    
-    
-    %%%%%%%%%%%%%%%%% augmentation part IMG    %%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    name=files(p).name;
-    token = strtok(name,'.');
-    
-    ss = DIR;
-    PATH = strcat(ss,name);
-    
-    im=imread(PATH);
-
-        %READ IMAGE 
-        %im=imread('img/2008_000026.jpg');
-        im = single(im);
-        I=zeros(size(im));
-        im2=zeros(size(im));
-        %RGB to BGR
-        im2 = im(:,:,[3 2 1]);
-        for c = 1:3
-            I(:, :, c) = im2(:, :, c) - mean_pix(c);
-        end
-
-
-        I = imresize(I, [IMAGE_DIM IMAGE_DIM]);
-
+for p = 1:imgs_num
         
-        %Crop image
-
-        % mean BGR pixel
-        mean_pix = [104.00698793, 116.66876762, 122.67891434];
-
-        %tic;
-        input_data = CROP(im, mean_pix);
-        %toc;
-
-        
-          %%%%%%%%%%%%%%%%% augmentation part SEG
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   
-    name2=files2(p).name;
-    token2 = strtok(name2,'.');
+    %%%%%%%%%%%%%%%%% augmentation part IMG %%%%%%%%%%%%%%%%%%%%%%%%%%
     
-    ss = DIR2;
-    PATH = strcat(ss,name2);
+    % get the path and name of the image
+    origin_img_name = origin_imgs(p).name; 
+    PATH_org_img = strcat(DIR_DATASET,origin_img_name);
     
-     MM=load(PATH);
-     M=MM.PartMask;    
-     M=resizeImage(M,IMAGE_DIM,1); 
+    % READ IMAGE 
+    % origin_img = imread('img/2008_000026.jpg');
+    origin_img = imread(PATH_org_img);
+    origin_img = imresize(origin_img, [CROP_DIM CROP_DIM],'bilinear')
+ 
+    origin_img = single(origin_img);
+    image = zeros(size(origin_img));
     
-              
-        %tic;
-        input_data = CROP_LABEL(M);
-        %toc;
-        
-        
-        
-        %%%%%%%%%%%%%%%%% augmentation part IMG
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
+    % img_BGR:BGR image
+    img_BGR = zeros(size(origin_img));
+    disp(size(origin_img)
+    
+    % RGB to BGR
+    img_BGR = origin_img(:,:,[3 2 1]);
+    
+    for c = 1:3
+        image(:, :, c) = img_BGR(:, :, c) - mean_pix(c);
+    end
+    
+        
+    %%%%%%%%%%%%%%%%% augmentation part SEG
+    
+    mask_img_name = mask_imgs(p).name;
+    PATH_mask_img = strcat(DIR_MASK,mask_img_name);
+    
+    
+    mask_img = imread(PATH_mask_img);
+    mask_img = imresize(mask_img, [CROP_DIM CROP_DIM],'bilinear');
+    mask_img = RGB2Mask(mask_img); 
+                    
+        
+    %%%%%%%%%%%%%%%%% augmentation part IMG---scaling and rotating
+       
     for i = 1:aug_num
+    
+        x = rand*8;
+        
+        %% scaling and rotating
+        
+        if(x>=0 && x<=2)
         
             % random scale and angle factor are calculated and given to the
-            % augment-functions
-             Y = str2double(sprintf('%.2f',(0.5)*rand)); 
-             scaleFactor = 0.7 + Y;
-            angle = rand*20;
+	    % augment-functions
+	    Y = str2double(sprintf('%.2f',(0.5)*rand)); 
+	    scaleFactor = 0.7 + Y;
+	    angle = rand*20;
            
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%IMG%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            finImage = augment(I, angle, scaleFactor);
-            
-            augData = single(finImage);
-            
-            augData=permute(augData, [2 1 3]);
-            
-            Img(:,:,:,i+(aug_num*index))=augData;
-            
-            %%%%%%%%%%%%%%%%%%%%%MASK%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            finMask = augment_Mask(M, angle, scaleFactor);
-            
-            augLabels = single(finMask);
-            
-            augLabels=permute(augLabels, [2 1 3]);
-            
-            Seg(:,:,:,i+(aug_num*index))=augLabels;
-            
-            
-
-    end   
-          
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%IMGS%%%%%%%%%%%%%%%%%%%%%%%%%%%
-       % for pos=1:11
-       %     Img(:,:,:,pos+i+(aug_num*index))=input_data(:,:,:,(pos));
-       % end
-    
-      
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%SEG%%%%%%%%%%%%%%%%%%%%%%%%%
-       % for pos=1:11
-       %     Seg(:,:,:,pos+i+(aug_num*index))=input_data(:,:,:,(pos));
-       % end
+	    %% img augmentation
+	    img_aug = augment(image, angle, scaleFactor);            
+	    img_aug = single(img_aug);            
+	    img_aug=permute(img_aug, [2 1 3]);            
+	    Img(:,:,:,i+(aug_num*index))=img_aug;
         
-        
-        
-        
-    
-    
-           index = index +1;
-           % clearvars augData;
-           % clearvars augLabels;
-        
-
-end
-%{
- DIR='masks/';
-
-files=dir(DIR);
-
-loop=length(files);
-
-Seg=zeros([CROP_DIM CROP_DIM 1 ((loop-2)*(aug_num+11))]);
-
-index=0;
-
-for p=3:loop
-    p-2
-    
-    name=files(p).name;
-    token = strtok(name,'.');
-    
-    ss = DIR;
-    PATH = strcat(ss,name);
-    
-     MM=load(PATH);
-     M=MM.PartMask;    
-     M=resizeImage(M,IMAGE_DIM,1); 
-    
-              
-        %tic;
-        input_data = CROP_LABEL(M);
-        %toc;
-
-        %create the global vector 
-        %for pos=1:11
-        %    Seg(:,:,:,pos+(11*index))=input_data(:,:,:,pos);
-        %end
-        for i = 1:aug_num
-        
-            % random scale and angle factor are calculated and given to the
-            % augment-functions
-             Y = str2double(sprintf('%.2f',(0.5)*rand)); 
-            scaleFactor = 0.7 + Y;
-            angle = rand*20;
-            %finImage = augment(I, angle, scaleFactor);
-            
-            %augData = single(finImage);
-            
-            finMask = augment_Mask(M, angle, scaleFactor);
-            
-            augLabels = single(finMask);
-            
-            augLabels=permute(augLabels, [2 1 3]);
-            
-            Seg(:,:,:,i+(aug_num*index))=augLabels;
-
-        end   
-        
-        for pos=(1+aug_num):((1+aug_num+11))
-            Seg(:,:,:,pos+(aug_num*index))=input_data(:,:,:,(pos-aug_num));
+	    %% mask augmentatin
+	    labels_aug = augment_Mask(mask_img, angle, scaleFactor);            
+	    labels_aug = single(labels_aug);            
+	    labels_aug = permute(labels_aug, [2 1 3]);            
+	    Seg(:,:,:,i+(aug_num*index))=labels_aug;
+	end
+	 
+	if (x>2 && x<=3)
+	
+	    %% color contrast
+	    img_aug = augment_color_contrast(image);
+            img_aug = single(img_aug);            
+	    img_aug = permute(img_aug, [2 1 3]);            
+	    Img(:,:,:,i+(aug_num*index))=img_aug;
+                
+            % mask
+	    labels_aug = single(mask_img);            
+	    labels_aug = permute(labels_aug, [2 1 3]);            
+	    Seg(:,:,:,i+(aug_num*index)) = labels_aug;
         end
+         
+        if(x>3 && x<=4)
         
-        
-        index=index+1;
-        
-end
+            %% Horizontal Flipping             
+            %figure;
+            %imshow(image);
+            %figure;
+            %imagesc(mask_img);
+                                
+            img_aug = fliplr(image);     
+            img_aug = single(img_aug);                
+            img_aug = permute(img_aug, [2 1 3]);
+            Img(:,:,:,i+(aug_num*index)) = img_aug;
+                           
+            % mask 
+            labels_aug = fliplr(mask_img);
+            labels_aug = single(labels_aug);
+            labels_aug = permute(labels_aug, [2 1 3]);
+            Seg(:,:,:,i+(aug_num*index)) = labels_aug;
 
-%}
+            %figure;
+            %imshow(img_aug);
+            %figure;
+            %imagesc(labels_aug);       
+        end
+         
+        if(x>4 && x<=5)
+        
+	    %% Skewing 
+	    %% Need to use the image without mean subtraction        
+            shx = rand*0.1;
+            shy = rand*0.1;
+            img_aug = skewing(img_BGR,shx,shy);            
+            img_aug = single(img_aug);
+
+            % SUBTRACT MEAN  
+            for c = 1:3
+                img_aug(:, :, c) = img_aug(:, :, c) - mean_pix(c);
+            end
+
+            img_aug = permute(img_aug, [2 1 3]);
+            Img(:,:,:,i+(aug_num*index)) = img_aug;           
+            
+            %% segmentation 
+            labels_aug = skewing(mask_img,shx,shy);           
+            labels_aug = single(labels_aug);
+            labels_aug = permute(labels_aug, [2 1 3]);
+            Seg(:,:,:,i+(aug_num*index)) = labels_aug;
+            % figure, imshow(img_aug);
+            % figure, imagesc(labels_aug); 
+        end
+         
+        if(x>5 && x<=6)
+        
+	    %% Vignetting 
+            scaleFactor = rand*0.6;
+            img_aug = Vignetting(img_BGR,scaleFactor);
+            img_aug = single(img_aug);
+
+            % SUBTRACT MEAN 
+            for c = 1:3
+                img_aug(:, :, c) = img_aug(:, :, c) - mean_pix(c);
+            end
+
+            img_aug = permute(img_aug, [2 1 3]);
+            Img(:,:,:,i+(aug_num*index)) = img_aug;
+                
+                
+            %% mask                    
+            labels_aug = single(mask_img);
+            labels_aug = permute(labels_aug, [2 1 3]);
+            Seg(:,:,:,i+(aug_num*index)) = labels_aug;
+            % figure, imshow(img_aug);
+            % figure, imagesc(labels_aug); 
+        end
+
+
+        if(x>6 && x<=8)
+             
+             [images] = CROP(img_BGR);
+             [masks] = CROP_LABEL(mask_img);
+             %for t=1:size(images,4)
+             %      figure;
+             %      imshow(images(:,:,:,t));
+             %      figure;
+             %      imagesc(masks(:,:,:,t));           
+             %end
+             %size(images)
+             %size(masks)
+             
+            selector = ceil(rand*11);             
+            img_aug = images(:,:,:,selector);
+            img_aug = single(img_aug);
+             
+            for c = 1:3
+                img_aug(:, :, c) = img_aug(:, :, c) - mean_pix(c);
+            end             
+             
+            %augData=permute(augData2, [2 1 3]);?
+            Img(:,:,:,i+(aug_num*index)) = img_aug;
+
+            %% mask
+            labels_aug = masks(:,:,:,selector);
+            labels_aug = single(labels_aug);
+            %augLabels=permute(augLabels, [2 1 3]);
+            Seg(:,:,:,i+(aug_num*index)) = labels_aug;       
+        end
+         
+                            
+    end                
+             
+    index = index +1;
+    % clearvars augData;
+    % clearvars augLabels;       
+end
 
 %Img is the data structure with the augmented data
 
- p = randperm(size(Img,4));
+p = randperm(size(Img,4));
  
- labels_caffe=zeros([CROP_DIM CROP_DIM 1 ((loop-2)*(aug_num))]);
+labels_caffe = zeros([CROP_DIM CROP_DIM 1 ((imgs_num)*(aug_num))]);
  
- for i=1:size(Img,4)
-    labels_caffe(:,:,:,i)=Seg(:,:,:,p(i));
- end
+for i=1:size(Img,4)
+    labels_caffe(:,:,:,i) = Seg(:,:,:,p(i));
+end
 
- clearvars Seg;
+clearvars Seg;
  
- data_caffe=zeros([CROP_DIM CROP_DIM 3  ((loop-2)*(aug_num))]);
+data_caffe = zeros([CROP_DIM CROP_DIM 3  ((imgs_num)*(aug_num))]);
   
- for i=1:size(Img,4)
-    data_caffe(:,:,:,i)=Img(:,:,:,p(i));
- end
+for i = 1:size(Img,4)
+    data_caffe(:,:,:,i) = Img(:,:,:,p(i));
+end
 
- clearvars Img;
+clearvars Img; 
  
+%hdf5write( ['TEST.h5'], 'data',ImgSuffle, 'label', SegSuffle);
+%createHDF5_smallSets;
+disp('Start ....... ');
+%%create small datasets
+
+ppp=100
+
+NETNAME = 'v33';
+nFiles = 20;
+
+%why not ceil 
+nAugmentedImages = round((round((imgs_num)/6)*(aug_num))/nFiles);
+%nAugmentedImages=1000;
+
+index = 1;
+for fi = 1:nFiles
+  
+    if(index+nAugmentedImages > (round((imgs_num)/6)*(aug_num)))
+	indexBegin = index;
+	indexEnd = (round((imgs_num)/6)*(aug_num)); 
+    else
+	indexBegin = index 
+	indexEnd = index+nAugmentedImages
+    end
+    index = index+nAugmentedImages
+  
+    clearvars img_aug;
+    clearvars labels_aug;
+      
+  
+    img_aug   = single(data_caffe(:,:,:,indexBegin:indexEnd));
+    labels_aug = single(labels_caffe(:,:,:,indexBegin:indexEnd));
+    %augLabels2 = single(labels_caffe2(:,:,:,indexBegin:indexEnd));
+    %augLabels3 = single(labels_caffe3(:,:,:,indexBegin:indexEnd));
+  
+    
+    disp('Creating randomly transformed image ');
+
+
+    hdf5write( ['aug_deformed_' NETNAME '_file' num2str(fi) '.h5'], ...
+                         'data', img_aug, ...
+                         'label', labels_aug);
+                       %  'label1', augLabels2, ...
+                       %  'label2', augLabels3);
+    
+  
+end
  
 
- %hdf5write( ['TEST.h5'], 'data',ImgSuffle, 'label', SegSuffle);
- createHDF5_smallSets;
-
+system(['echo aug_deformed_' NETNAME '_file*.h5 > aug_deformed_' ...
+                NETNAME '.txt'])
--- augmentation_scripts/calculate_mean_pix.m
+++ augmentation_scripts/calculate_mean_pix.m
@@ -1,15 +1,21 @@
-clear all;
-close all;
+%this function return the BGR 3 channel mean value for images--jing
+% Do we need to crop the image before we get the mean value
+function img_mean =calculate_mean_pix(DIR,origin_imgs, image_dim,final_dim)
 
-IMAGE_DIM=300;
-FINAL_DIM = 300;
+IMAGE_DIM=image_dim;
+FINAL_DIM=final_dim;
+%IMAGE_DIM=300;
+%FINAL_DIM = 300;
 % mean BGR pixel
 
 
-imaDIR='/home/valada/test/nrg/';
-imafiles=dir(imaDIR);
-loop=length(imafiles);
-display(loop);
+%imaDIR='/home/valada/test/nrg/';
+%DIR_DATASET='/home/valada/test/nrg/
+DIR_DATASET=DIR;
+
+%origin_imgs=dir(DIR_DATASET);
+imgs_num=length(origin_imgs);
+display(imgs_num);
 blue = 0;
 green = 0;
 red = 0;
@@ -17,41 +23,36 @@ red = 0;
 index=0;
 
 %loop first 10
-for p=3:loop
-    p-2
-    
-    %%%%%%%%%%%% Image part
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    name=imafiles(p).name;
-    token = strtok(name,'.');
-    ss = imaDIR;
-    PATH = strcat(ss,name);
-    
-    ima=imread(PATH);
+% image 1-(loop-2)--jing
+for p=1:imgs_num        
 
-    ima = single(ima); 
-    % resize to fixed input size
-    ima = imresize(ima, [IMAGE_DIM IMAGE_DIM], 'bilinear');
+    origin_img_name = origin_imgs(p).name;
+    PATH_org_img = strcat(DIR_DATASET,origin_img_name);    
+    origin_img = imread(PATH_org_img);
+    origin_img = single(origin_img); 
+    
+    % resize to fixed input size, here why is not final_dim
+    % origin_img = imresize(origin_img, [FINAL_DIM FINAL_DIM], 'bilinear');
+    origin_img = imresize(origin_img, [FINAL_DIM FINAL_DIM], 'bilinear');
     % permute from RGB to BGR (IMAGE_MEAN is already BGR)
-    size(ima);
+    % why size?
+    size(origin_img);    
     
-    
-    for i = 1:IMAGE_DIM
-        for j = 1:IMAGE_DIM
-            blue = blue + ima(i,j,3);
-            green = green + ima(i,j,2);
-            red = red + ima(i,j,1);
+    for i = 1:FINAL_DIM
+        for j = 1:FINAL_DIM
+            blue = blue + origin_img(i,j,3);
+            green = green + origin_img(i,j,2);
+            red = red + origin_img(i,j,1);
         end
     end
     
 end
-cur = IMAGE_DIM*IMAGE_DIM*(loop-2);
+cur = FINAL_DIM*FINAL_DIM*(imgs_num);
 BLUE = blue/cur;
 GREEN = green/cur;
 RED = red/cur;
 
+img_mean=[BLUE GREEN RED];
 
     
     
